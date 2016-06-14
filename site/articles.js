08d1bdb842ddcda5534cd4161c4bb03eba4ecb07
metasophiea <metasophiea@gmail.com>
2016-06-14 20:59:15 +0100

    Update

--- site/articles.js
+++ site/articles.js
@@ -4,7 +4,7 @@ var articles = {
 		"text_line1":"check out the lastest from the fresh graphics engine on the block",
 		"text_line2":"",
 		"backerFill":"rgba(179,102,255,0.85)",
-		"image_url":"file:///home/metasophiea/Software/metasophiea.github.io/site/images/lavendelproject.png",
+		"image_url":"http://metasophiea.com/site/images/lavendelproject.png",
 		"link_url":"http://metasophiea.com/lavendel/info"
 	},
 	"article_20160606-a":{
