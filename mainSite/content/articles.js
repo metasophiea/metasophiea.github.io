c386e51a8d233f7b4090d17bdb79ac4009c8cdad
metasophiea <metasophiea@gmail.com>
2016-08-05 21:59:18 +0100

    updated main articles

--- mainSite/content/articles.js
+++ mainSite/content/articles.js
@@ -1,20 +1,12 @@
 var articles = {
-	"article_20160606-b":{
-		"headline":"The Lavendel Project",
-		"text_line1":"check out the lastest from the fresh graphics engine on the block",
-		"text_line2":"",
-		"backerFill":"rgba(179,102,255,0.85)",
-		"image_url":"http://metasophiea.com/lavendel/resources/images/lavendelproject.png",
-		"link_url":"http://metasophiea.com/lavendel/info"
-	},
 	"article_20160606-a":{
-		"headline":"Presenter",
-		"text_line1":"Take your presentation and throw it out!",
-		"text_line2":"This is the rock and roll of slide shows",
+		"headline":"Canvas Viewing Space Project",
+		"text_line1":"A pure JS 2d graphics engine",
+		"text_line2":"currently developing into dynamic presentation app",
 		"textcolour":"rgb(255,255,255)",
 		"backerFill":"rgba(0,0,0,0.75)",
-		"image_url":"http://metasophiea.com/presenter/resources/images/PresenterSplash_1.bmp",
-		"link_url":"http://metasophiea.com/apps/presenter/info"
+		"image_url":"http://metasophiea.com/lib/image/canvasViewingSpace_splash1.bmp",
+		"link_url":"http://metasophiea.com/lib/js/canvasViewingSpace"
 	},
 	"article_20160614-a":{
 		"headline":"Free Code Camp Project Archive",
