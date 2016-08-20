fe538d8efb5ed945ca33a75e0f46a7816f8b1d5a
metasophiea <metasophiea@gmail.com>
2016-08-20 22:33:18 +0100

    201608202232 website development

--- mainSite/content/articles2.js
+++ mainSite/content/articles2.js
@@ -1,40 +1,44 @@
-var articles = {
-	"article_20160807-a":{
+var articles = [
+	{	"id":"article_20160807-a",
 		"headline":"Canvas Viewing Space Project",
-		"text_line1":"A pure JS 2d graphics engine",
-		"text_line2":"currently developing into dynamic presentation app",
+		"text":["A pure JS 2d graphics engine, currently developing into dynamic presentation app"],	
 		"textcolour":"rgb(255,255,255)",
 		"backerFill":"rgba(0,0,0,0.75)",
 		"image_url":"http://metasophiea.com/lib/image/canvasViewingSpace_splash1.bmp",
 		"link_url":"http://metasophiea.com/lib/js/canvasViewingSpace"
 	},
-	"article_20160807-b":{
+	{	"id":"article_20160807-b",
 		"headline":"Word Stats",
-		"text_line1":"A mini webapp showing you some info on things you write in",
-		"text_line2":"not particularly useful..but somewhat handy n_n",
+		"text":["A mini webapp showing you some info on things you write in ... not particularly useful..but somewhat handy n_n"],
 		"textcolour":"rgb(255,255,255)",
 		"backerFill":"rgba(0,0,100,0.75)",
 		"image_url":"http://metasophiea.com/lib/image/wordstats_splash1.bmp",
 		"link_url":"http://metasophiea.com/apps/wordstats"
-	},
-	"article_20160614-a":{
+	},	
+	{	"id":"article_20160614-a",
 		"headline":"Free Code Camp Project Archive",
-		"text_line1":"check out some of my original websites",
-		"text_line2":"from when I was first learning HTML, JS and CSS",
+		"text":["check out some of my original websites from when I was first learning HTML, JS and CSS"],
 		"textcolour":"rgb(0,0,0)",
 		"backerFill":"rgba(255,255,255,0.75)",
 		"image_url":"http://newbornstartups.com/wp-content/uploads/2015/06/fcc-logo.png",
 		"link_url":"http://metasophiea.com/sideProjects/FreeCodeCamp/"		
 	},
-	"article_20160616-a":{
+	{	"id":"article_20160616-a",
 		"headline":"Curve and Curve 2 Beta",
-		"text_line1":"my soundcloud, where I store 2 of my latest musical works, i haven't",
-		"text_line2":"worked on this for a while..unfortunately (I'm in a band now though)",
+		"text":["my soundcloud, where I store 2 of my latest musical works, i haven't worked on this for a while..unfortunately (I'm in a band now though)"],
 		"textcolour":"rgb(0,0,0)",
 		"backerFill":"rgba(255,128,60,0.65)",
 		"image_url":"http://4.bp.blogspot.com/-D57hGYq5DY0/VnhfsqXYZbI/AAAAAAAAKyg/YXj6uQf8DnU/s1600/curve_1.bmp",
 		"link_url":"https://soundcloud.com/metasophiea/"		
 	},
-};	
+	{	"id":"article_20160820-a",
+		"headline":"Github Repository for this Website",
+		"text":["Ever wonder how all this magic works? Turns out its a controlled mess! Check it out for yourself on the github.com repo"],
+		"textcolour":"rgb(0,0,0)",
+		"backerFill":"rgba(245,245,245,0.65)",
+		"image_url":"http://metasophiea.com/lib/image/metasophiea.github.io.png",
+		"link_url":"https://github.com/metasophiea/metasophiea.github.io"		
+	},	
+];
 
 console.log('%c'+Object.keys(articles).length+' articles loaded', 'color:rgb(202,136,202); font-style:italic;');
